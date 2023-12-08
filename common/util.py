25dcf7d77a91a2a6802ea9bd87438ee9458b5750
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2023-12-08 18:29:30 +0800

    Beautify

--- common/util.py
+++ common/util.py
@@ -8,8 +8,10 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
+# 进货量上限
 stock_stand = 10000
-pricing_stand = 100
+# 进价上限
+pricing_stand = 50
 
 
 def load_data():
@@ -59,9 +61,8 @@ def fitness_function(coefficients, individual, AttritionRate, WholesalePrices):
     stock_counts, pricings = individual[:len(individual) // 2], individual[len(individual) // 2:]
 
     # 卖出量
-    sale_counts = np.array([sum([one * math.pow(pricing, i + 1) for i, one in enumerate(coefficient)])
+    sale_counts = np.array([sum([one * math.pow(pricing, i) for i, one in enumerate(coefficient)])
                             for coefficient, pricing in zip(coefficients, pricings)])
-
     # 损耗量 = 损耗率 * 进货量
     loss_counts = AttritionRate * stock_counts
     # 卖出量 = min(卖出量, 进货量 - 损耗量)
@@ -71,18 +72,17 @@ def fitness_function(coefficients, individual, AttritionRate, WholesalePrices):
     cost = stock_counts * WholesalePrices
     # 销售量 = 卖出量 * 定价
     sales = sale_counts * pricings
-
     # 利润 = 销售量 - 成本
     profit = sum(sales) - sum(cost)
 
     return profit
 
 
-def crossover(fitness, population, crossover_rate):
+def crossover(population, fitness, crossover_rate):
     # 适应概率
     probabilities = [one / sum(fitness) for one in fitness]
     # 根据适应概率选取父本
-    parents = random.choices(population, probabilities, k=int(len(population) * crossover_rate))
+    parents = random.choices(population, probabilities, k=int(len(population) * crossover_rate * 2))
 
     # 确定交叉点
     cross_point = np.random.randint(1, len(parents[0]))
@@ -107,12 +107,13 @@ def mutate(children, mutation_rate):
     return children
 
 
-def copy(population, fitness, top):
+def select(population, fitness, top):
     # 捆绑适应度与群体
-    back = [(fitness, individual) for fitness, individual in zip(fitness, population)]
+    fitness_individual = [(fitness, individual) for fitness, individual in zip(fitness, population)]
+    # 根据适应度对群体排序，选择前top名
+    selected = [one[1] for one in sorted(fitness_individual, key=lambda x: -x[0])[:top]]
 
-    # 根据适应度对群体排序，返回前top名
-    return [one[1] for one in sorted(back, key=lambda x: -x[0])[:top]]
+    return selected
 
 
 def xlsx_to_csv(paths):
